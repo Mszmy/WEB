ebc8e6345760985aa3c879e41ad8ed33d80c0287
wanjun_Y <46343874+Mszmy@users.noreply.github.com>
2021-06-28 08:58:58 +0800

    Create App.vue

---
+++ VUE/12-webpack/12.1-webpack的使用/12.1.4-webpack配置vue/src/vue/App.vue
@@ -0,0 +1,23 @@
+<template>
+    <div class="title">
+        {{message}}
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "App",
+        data(){
+            return{
+                message: 'hello, webpack-- app.vue'
+            }
+        }
+    }
+</script>
+
+<!--样式-->
+<style scoped>
+    .title{
+        color:green;
+    }
+</style>
